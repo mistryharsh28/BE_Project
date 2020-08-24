98fe2867e031ad2d8f64b3d2df524176ecedb807
Harsh <mistryharsh28@gmail.com>
2020-08-24 16:26:33 +0530

    Add login and signup frontend

--- Web_Application/server.js
+++ Web_Application/server.js
@@ -8,18 +8,84 @@ const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
   debug: true
 });
+const mongoose = require('mongoose');
 const { v4: uuidV4 } = require('uuid')
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+
+
+mongoose.connect("mongodb+srv://qwerty:qwerty@123@be-project.llqsi.mongodb.net/BE-Project?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+  console.log('Mongo Connection Successful');
+}).catch(err => {
+  console.log('ERROR:', err.message);
+})
+
+
+var UserSchema = new mongoose.Schema(
+  { 
+   user_name: {
+        type: String,
+        require: true
+      },
+   user_password: {
+        type: String,
+        require: true
+      },
+   email:  {
+        type: String,
+        require: true
+      }
+},{collection: 'users'});
+
+var Users = mongoose.model('Users',UserSchema);
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  console.log(req.body);
+
+  res.redirect('/login')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register', (req, res) => {
+  var username = req.body.name;
+  var email = req.body.email;
+  var contact = req.body.contact;
+  var password = req.body.password;
+  var confirm_password = req.body.confirm_password;
+
+  
+
+  console.log(req.body);
+  
+
+  res.redirect('/register')
+})
+
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
 
-app.get('/:room', (req, res) => {
+app.get('/room/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
@@ -38,5 +104,7 @@ io.on('connection', socket => {
     })
   })
 })
+
+
 
 server.listen(process.env.PORT||3030)
